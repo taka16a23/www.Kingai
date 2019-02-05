2c641609ecd1104c88e177c55377976c6c6cfe9a
Atami <taka16a23@gmail.com>
2019-02-05 22:13:06 +0900

    Change datetime format for admin page

--- www/Kintai/admin/dakoku.py
+++ www/Kintai/admin/dakoku.py
@@ -12,14 +12,37 @@ class DakokuAdmin(admin.ModelAdmin):
     DakokuAdmin is a admin.ModelAdmin.
     Responsibility:
     """
+    TIME_FORMAT = "%y年%m月%d日 %H時%M分%S秒"
+
     list_display = ('dakokuID',
                     'userID',
-                    'dateTime',
+                    'format_dateTime', # dateTime
                     'dakokuStatusID',
                     'dakokuMethodID',
                     'Bikou',
                     'sakujoFlag'
     )
+
+    def format_dateTime(self, obj):
+        """SUMMARY
+
+        format_dateTime(obj)
+
+        @Arguments:
+        - `obj`:
+
+        @Return:
+
+        @Error:
+        """
+        # require
+
+        # do
+        return obj.dateTime.strftime(self.TIME_FORMAT)
+        # ensure
+
+    format_dateTime.admin_order_field = "dateTime"
+    format_dateTime.short_description = "打刻日時"
 
 
 
