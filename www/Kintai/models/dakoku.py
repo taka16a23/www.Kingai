465179cc79c7023635897c2c7ae9be806d3c6224
Atami <taka16a23@gmail.com>
2019-02-07 20:58:08 +0900

    Add Analyzer for dakoku

--- www/Kintai/models/dakoku.py
+++ www/Kintai/models/dakoku.py
@@ -5,13 +5,36 @@ r"""dakoku -- DESCRIPTION
 """
 from __future__ import unicode_literals
 
+from collections import Iterable
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.query import QuerySet
 from django.db.models import Manager, Q
 
+
+
 import dakokuKubun
 import dakokuMethod
+
+
+class DakokuAnalyzer(object):
+    """DakokuAnalyzer
+
+    DakokuAnalyzer is a object.
+    Responsibility:
+    """
+    def __init__(self, dakokus):
+        """
+
+        @Arguments:
+        - `dakokus`:
+        """
+        if not isinstance(dakokus, (Iterable, )):
+            raise TypeError('dakoku must be Iterable')
+        self._dakokus = dakokus
+
+
 
 
 class ManagerAbstract(Manager):
